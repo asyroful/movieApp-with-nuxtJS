827ae2944e15a7f4d2e613388107a622518105bc
Muhammmad Asyroful <asyroful.muna12@gmail.com>
2022-07-08 15:43:48 +0700

    first commit

---
+++ pages/movies/_movieid.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="single-movie container">
+    <NuxtLink class="button" :to="{ name: 'index' }">Back</NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'singleMovie'
+}
+</script>
+
+<style>
+
+</style>
